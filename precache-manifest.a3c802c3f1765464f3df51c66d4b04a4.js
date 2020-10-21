15d4121c3d4d61fcda4a35b8db4759d6a0ee274d
Grzywa1234 <47593899+Grzywa1234@users.noreply.github.com>
2020-10-21 15:54:03 +0200

    Add files via upload

---
+++ precache-manifest.a3c802c3f1765464f3df51c66d4b04a4.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5679f2543e2fb0a580583a46c1a08bc9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "252d4ca6f0d5f021284b",
+    "url": "/static/css/main.d8a8b2f4.chunk.css"
+  },
+  {
+    "revision": "4a9cd869962b30646bdc",
+    "url": "/static/js/2.fc467fd0.chunk.js"
+  },
+  {
+    "revision": "48866d4409b9b56662b9014bd540ee2b",
+    "url": "/static/js/2.fc467fd0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "252d4ca6f0d5f021284b",
+    "url": "/static/js/main.37be1c17.chunk.js"
+  },
+  {
+    "revision": "3e592ef704de36790278",
+    "url": "/static/js/runtime-main.033c7639.js"
+  },
+  {
+    "revision": "f55f08afe0449429c934e256af8a6e41",
+    "url": "/static/media/andres-dallimonti-kjqTlMHLci4-unsplash.f55f08af.jpg"
+  },
+  {
+    "revision": "edaa394d4da3608187f42c505ecd353e",
+    "url": "/static/media/shaun-darwood-TC6u_HnDDqs-unsplash2.edaa394d.jpg"
+  }
+]);
